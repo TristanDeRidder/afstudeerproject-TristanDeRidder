7bc5638fc9a1d28463f8ecc3e4dbfb2c284af1b6
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2025-02-12 13:42:36 +0100

    Install Remix & Test changes Strapi

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,20 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksRichText extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_rich_texts';
+  info: {
+    displayName: 'RichText';
+    icon: 'quote';
+  };
+  attributes: {
+    content: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.rich-text': BlocksRichText;
+    }
+  }
+}
