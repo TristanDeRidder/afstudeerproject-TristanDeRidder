8dcf48bb7eecfbf1791e137b5fe1299c6cd9dcb1
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2025-02-16 14:33:10 +0100

    Styling title

--- frontend/app/components/design/Title/PrimaryTitle.tsx
+++ frontend/app/components/design/Title/PrimaryTitle.tsx
@@ -1,5 +1,5 @@
 export default function PrimaryTitle({ title }: { title: string }) {
     return (
-        <h1 className="bold">{title}</h1>
+        <h1 className="text-3xl text-center mt-10">{title}</h1>
     )
 }
