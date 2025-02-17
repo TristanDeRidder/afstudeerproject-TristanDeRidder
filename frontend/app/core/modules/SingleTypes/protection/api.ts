bceaecb06ccf1498bbe57a77c14bf4a0b0e8fe0b
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2025-02-17 11:05:32 +0100

    API calls for single types

---
+++ frontend/app/core/modules/SingleTypes/protection/api.ts
@@ -0,0 +1,24 @@
+// Module
+import qs from 'qs';
+
+// Core
+import API from '../../../networking/API.server';
+
+// Type
+
+export async function getProtectionPage() {
+    const query = qs.stringify({
+
+    }, { 
+        encodeValuesOnly: true,
+    });
+
+    try {
+        const response = await API.get(`protection?${query}`);
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
