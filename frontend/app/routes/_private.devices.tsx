cd6635f390dca0632bd6706dee3c0c0f04f2208a
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2025-02-23 15:19:15 +0100

    Devices table

--- frontend/app/routes/_private.devices.tsx
+++ frontend/app/routes/_private.devices.tsx
@@ -1,7 +1,51 @@
+import { useLoaderData } from "@remix-run/react";
+import { getDevices } from "../core/modules/devices/api";
+import { Devices } from "../core/modules/devices/type";
+
+type LoaderData = {
+  devices: Devices[];
+};
+
+export async function loader() {
+  try {
+    const devices = await getDevices();
+
+    if (!devices?.data) {
+      throw new Error("No data available");
+    }
+
+    return { devices: devices.data };
+  } catch (error) {
+    console.error("Error while fetching data:", error);
+    return { devices: [] };
+  }
+}
+
 export default function Devices() {
+  const { devices } = useLoaderData<LoaderData>();
+
   return (
     <div>
-      <p>Test</p>
+      <table className="table-auto w-full">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Type</th>
+            <th className="px-4 py-2">Brand</th>
+            <th className="px-4 py-2">Model</th>
+          </tr>
+        </thead>
+        <tbody>
+          {devices.map((device) => (
+            <tr key={device.id}>
+              <td className="border px-4 py-2">{device.Type}</td>
+              <td className="border px-4 py-2">{device.brand?.BrandName}</td>
+              <td className="border px-4 py-2">
+                {device.Model} {device.ModelType}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
