f35db705549051c800e4c3cd45c464b79892ab17
pgm-tristanderidder <tristan.deridder@student.arteveldehs.be>
2025-02-24 13:50:40 +0100

    Data for order (403 error)

--- frontend/app/routes/_private.orders.tsx
+++ frontend/app/routes/_private.orders.tsx
@@ -1,7 +1,40 @@
+import { getOrders } from "../core/modules/orders/api";
+import type { Orders } from "../core/modules/orders/type";
+import { useLoaderData } from "@remix-run/react";
+
+
+type LoaderData = {
+  orders: Orders[];
+}
+
+export async function loader() {
+  try {
+    const orders = await getOrders();
+
+    if (!orders?.data) {
+      throw new Error("No data available");
+    }
+
+    return { orders: orders.data };
+  } catch (error) {
+    console.error("Error while fetching data:", error);
+    return { orders: [] };
+  }
+};
+
 export default function Orders() {
+  const { orders } = useLoaderData<LoaderData>();
+
   return (
     <div>
-      <p>Test</p>
+      <ul>
+        {orders.map((order) => (
+          <li key={order.id}>
+            <h2>{order.id}</h2>
+            <p>{order.OrderStatus}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
